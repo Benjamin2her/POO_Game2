f1c30912b208c59f443d61a974bf7622ee3e3314
Benjamin2her <benjamin.hernandez.herrera@uabc.edu.mx>
2021-12-04 18:02:05 -0800

    Proyecto final POO definitivo v2

---
+++ core/src/poo/objects/Player.java
@@ -0,0 +1,22 @@
+package poo.objects;
+
+import com.badlogic.gdx.graphics.Texture;
+
+// CLASE Player QUE HEREDA DE CLASE Object
+public class Player extends Object {
+
+    // CONSTRUCTORES SOBRECARGADOS
+    public Player(int x, int y, int w, int h, Texture img){
+        super(x, y, w, h, img);
+        }
+
+    public Player(int x, int y, Texture img){
+        super(x, y, 64, 64, img);
+    }
+
+    public void finalize(){
+        super.finalize();
+        System.out.println("Player destroyed");
+    }
+
+}
