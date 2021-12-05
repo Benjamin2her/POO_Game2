14ec47f10f8194392015f1efa448b8fc0bd45784
Benjamin2her <benjamin.hernandez.herrera@uabc.edu.mx>
2021-12-05 15:51:17 -0800

    Se añadio la clase camion

--- core/src/poo/objects/Camion.java
+++ core/src/poo/objects/Camion.java
@@ -1,4 +1,29 @@
 package poo.objects;
 
-public class Camion {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+
+// CLASE Camion QUE HEREDA DE CLASE Object
+public class Camion extends Object {
+
+    // CONSTRUCTORES SOBRECARGADOS
+    public Camion(int x, int y, int w, int h, Texture img){
+        super(x, y, w, h, img);
+    }
+
+    public Camion(int x, int y, Texture img){
+        super(x, y, 64, 64, img);
+    }
+
+    protected void finalize(){
+        super.finalize();
+        System.out.println("Camion destroyed");
+    }
+
+    // SOBRESCRIBIR METODO VIRTUAL DE CLASE Object, PARA SER USADO EN POLIMORFISMO
+    @Override
+    public void speed(){
+        this.y -= 200 * Gdx.graphics.getDeltaTime();
+    }
+
 }
